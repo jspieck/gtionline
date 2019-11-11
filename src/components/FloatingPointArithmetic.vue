4925f34a3190805d10628e981ae7de36190873d8
Marcel Brand <marcel.brand@fau.de>
2019-11-11 15:15:57 +0100

    Fuege Tabs fuer die Floating-Point-Aufgaben hinzu

--- src/components/FloatingPointArithmetic.vue
+++ src/components/FloatingPointArithmetic.vue
@@ -1,28 +1,73 @@
 <template>
-  <div class="fp-arithmetic">
-    Floating Point Stuff.
+<vue-tiny-tabs id="extabs" :anchor="false" :closable="false" :hideTitle="false" @on-close="onClose"
+  @on-before="onBefore" @on-after="onAfter">
+  <div class="section" id="exercises">
+    <h3 class="title">Übungsaufgaben</h3>
+    stuff
   </div>
+  <div class="section" id="free">
+    <h3 class="title">Freie Berechnung</h3>
+    mehr stuff
+  </div>
+</vue-tiny-tabs>
 </template>
 
 <script>
+import VueTinyTabs from 'vue-tiny-tabs';
+
 export default {
-  name: 'FloatingPointArithmetic',
+  name: 'TinyTabs',
+  components: {
+    'vue-tiny-tabs': VueTinyTabs,
+  },
+  methods: {
+    onClose(id) {
+      console.log('Callback function that gets evaluated while closing the tab', id);
+    },
+    onBefore(id, tab) {
+      console.log('Callback function that gets evaluated before a tab is activated', id, tab);
+    },
+    onAfter(id, tab) {
+      console.log('Callback function that gets evaluated after a tab is activated', id, tab);
+    },
+  },
 };
+
 </script>
 
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+.tinytabs .tabs {
+  margin-left: 15px;
+  display: flex;
+  flex-flow: row wrap;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.tinytabs .tabs .tab .close {
+  padding-left: 5px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.tinytabs {
+  margin: auto;
+  width: 640px;
 }
-a {
-  color: #42b983;
+.tinytabs .tabs .tab {
+  margin: 0 3px 2px 0;
+  background: #fcfcfc;
+  display: block;
+  padding: 6px 15px;
+  text-decoration: none;
+  color: #666;
+  font-weight: bold;
+  border-radius: 3px 3px 0 0;
+}
+.tinytabs .section {
+  background: #f5f5f5;
+  overflow: hidden;
+  padding: 15px;
+  clear: both;
+  border-radius: 3px;
+}
+.tinytabs .tab.sel {
+  background: #f5f5f5;
+  color: #333;
+  text-shadow: none;
 }
 </style>
