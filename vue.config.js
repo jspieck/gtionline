5555bf310ab726edb3946d71c8700f504a5867f3
Jan Spieck <jan.spieck@fau.de>
2024-10-17 09:50:33 +0200

    Deploy path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/gtionline' : '/',
   chainWebpack: (config) => {
     config.module
       .rule('fonts')
